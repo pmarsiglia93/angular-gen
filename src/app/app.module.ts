33753303178fba2f87b138a08bd838fabcafe583
Paulo <pmarsiglia93@gmail.com>
2022-02-17 10:53:10 -0300

    @pmarsiglia93 [TASK: Add Components]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastrarComponent,
+    EntrarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
