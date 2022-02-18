93654ea3b441101879ed88c788c0b4efb0446c27
pmarsiglia93 <pmarsiglia93@gmail.com>
2022-02-18 10:49:20 -0300

    @pmarsiglia93 [TASK: ROTAS Entrar e Cadastrar]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Contrução de uma Rota.
+  //O nome do Path sou eu que defino! (Melhor colocar parecido para facilitar a leitura).
+  //Libero uma nova tag dentro do Angular.
+  //Menu e Rodape não precisa de rota pois eles sempre vão estar lá.
+  //Nova Tag AppComponent Html.
+
+  //Forçando a pessoa a entrar no caminho entrar.
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+  
+  /*{path:'entraradm', component: EntrarComponent}, 
+  */
+
+  //Rotas
+  {path:'entrar', component: EntrarComponent}, 
+  {path:'cadastrar', component: CadastrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
